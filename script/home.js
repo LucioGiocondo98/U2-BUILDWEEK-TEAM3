98eecf633863e5cb76d0263e0c4de439b61c18ed
feliceliparuli <liparuli.felice50@gmail.com>
2025-03-26 16:58:53 +0100

    aggiornato href home che ora porta alla pagina dell'album, creato html e css in album page per ospitare i dettagli di ogni album

--- script/home.js
+++ script/home.js
@@ -1,23 +1,26 @@
-const albumIds = [75621062, 315283, 302127, 707965, 673968231, 109943, 6941024, 14521228];
+const albumIds = [
+  75621062, 315283, 302127, 707965, 673968231, 109943, 6941024, 14521228,
+];
 const albumContainer = document.getElementById("album-container");
 
-albumIds.forEach(albumId => {
-    const albumUrl = `https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`;
+albumIds.forEach((albumId) => {
+  const albumUrl = `https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`;
 
-    fetch(albumUrl)
-        .then(response => response.json())
-        .then(data => {
-            const col = document.createElement('div');
-            col.classList.add('col-6', 'col-md-4', 'col-lg-3', 'h-auto', 'g-3');
+  fetch(albumUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("album", data);
+      const col = document.createElement("div");
+      col.classList.add("col-6", "col-md-4", "col-lg-3", "h-auto", "g-3");
 
-            // Creazione card
-            const card = document.createElement('div');
-            card.classList.add('card', 'album-card', 'bg-secondary', 'text-white');
-            card.style.height = '260px';
-            card.style.cursor = 'pointer';
+      // Creazione card
+      const card = document.createElement("div");
+      card.classList.add("card", "album-card", "bg-secondary", "text-white");
+      card.style.height = "260px";
+      card.style.cursor = "pointer";
 
-            // Aggiungere contenuto alla card
-            card.innerHTML = `
+      // Aggiungere contenuto alla card
+      card.innerHTML = `
                 <img src="${data.cover_medium}" class="card-img-top" alt="Copertina dell'album ${data.title}">
                 <div class="card-body bg-dark">
                     <h2 class="card-title h6">${data.title}</h2>
@@ -26,12 +29,12 @@ albumIds.forEach(albumId => {
                     </p>
                 </div>
             `;
-            card.addEventListener("click", () => {
-                window.location.href = `artist.html?artistId=${data.artist.id}`;
-            });
+      card.addEventListener("click", () => {
+        window.location.href = `albumpage.html?Id=${data.id}`;
+      });
 
-          col.appendChild(card);
-            albumContainer.appendChild(col);
-        })
-        .catch(error => console.error('Errore durante la fetch:', error));
+      col.appendChild(card);
+      albumContainer.appendChild(col);
+    })
+    .catch((error) => console.error("Errore durante la fetch:", error));
 });
